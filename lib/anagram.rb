f738c231bce10022142b6b9e1d385d4c5c624233
Danny Rivera <samboard@hotmail.com>
2021-02-10 15:50:30 -0500

    commit tree 9c0fdc35c434e6c12c2e09f7badd8ba683f10322

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word, :match
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(phrase)
+        @phrase = phrase
+        @response = []
+        @phrase.each do |each_word|
+            if @word.split("").sort == each_word.split("").sort
+                @response << each_word
+            end
+        end
+        @response
+    end
+
+end
